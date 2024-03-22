937bcbdd9b3c1383ed68e419a7f25146b302cd6b
kcapka <147353671+kcapka@users.noreply.github.com>
2024-03-21 17:36:22 -0700

    started About page

---
+++ app/routes/about.tsx
@@ -0,0 +1,12 @@
+
+
+export default function About() {
+
+    return (
+        <div>
+            <section className="h-[50vh] md:h-[70vh] relative overflow-hidden">
+                <img src='images/roller-blue.jpeg' alt="Paint roller" className="w-[200vw] md:w-full min-h-full absolute md:top-[-30%] xl:top-[] left-0" />
+            </section>
+        </div>
+    )
+}
